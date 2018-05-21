a9694b3236d6a94e7f62eeef58cd240ea0473235
rmbozhko <yamakasi.romka@yahoo.fr>
2018-05-21 12:23:05 +0200

    * Added elements voice spelling & adaptive elements names
    * With Google URL Shortener Service added playing audio tracks from Wiki
    when card is turn to front side.
    * If element name exceed some constant value its font-size will be
    reduced accordingly.

--- d00/ex03/mendeleiev.js
+++ d00/ex03/mendeleiev.js
@@ -1,14 +1,33 @@
 var cards = document.querySelectorAll('.card');
+// var audio = document.getElementsByClassName("audio");
+// console.log(audio, audio.length);
+// console.log(cards);
+// console.log(cards[11].firstElementChild.childNodes[1].src = "rf.mp3");
+
+function		play(track_name)
+{
+	// cards[11].firstElementChild.childNodes[1].src = track_name
+	new Audio(track_name).play();
+	// for (var i = 0; i < audio.length; i++) {
+	// 	audio[i].addEventListener("click", function () { item.play(); });
+	// }
+}
 
 if (cards)
 {
 	console.log("Elements successfully found.");
-	
 	cards.forEach(function (item, idx) {
         item.addEventListener('click', function () {
             this.classList.toggle('is-flipped');
         });
     });
+
+    for (var i = 0; i < cards.length; i++) {
+		var text = cards[i].innerText.split('\n')[0];
+		console.log(text, text.length);
+		if (text.length >= 10)
+			cards[i].firstElementChild.style.fontSize = "75%";
+	}
 }
 else
 {
